ce149e034b882c69f0216e72ade3005babe6b3e5
Jamesmburu899 <jmaina1@byupathway.edu>
2025-04-12 19:35:03 +0300

    add

--- controllers/invController.js
+++ controllers/invController.js
@@ -3,28 +3,48 @@ const utilities = require("../utilities/")
 
 const invCont = {}
 
+// Build inventory by classification view
 invCont.buildByClassification = async function (req, res, next) {
-  const classificationId = req.params.classificationId
-  const data = await invModel.getInventoryByClassificationId(classificationId)
-  const grid = await utilities.buildClassificationGrid(data)
-  let nav = await utilities.getNav()
-  res.render("./inventory/classification", {
-    title: data[0].classification_name + " vehicles",
-    nav,
-    grid,
-  })
+  try {
+    const classificationId = req.params.classificationId
+    const data = await invModel.getInventoryByClassificationId(classificationId)
+    const grid = await utilities.buildClassificationGrid(data)
+    let nav = await utilities.getNav()
+    const className = data[0].classification_name
+    res.render("./inventory/classification", {
+      title: className + " Vehicles",
+      nav,
+      grid,
+    })
+  } catch (error) {
+    error.status = 500
+    next(error)
+  }
 }
 
+// Build inventory detail view
 invCont.buildByInventoryId = async function (req, res, next) {
-  const inventoryId = req.params.inventoryId
-  const data = await invModel.getInventoryByInventoryId(inventoryId)
-  const vehicleDisplay = await utilities.buildVehicleDetail(data[0])
-  let nav = await utilities.getNav()
-  res.render("./inventory/detail", {
-    title: data[0].inv_make + " " + data[0].inv_model,
-    nav,
-    vehicleDisplay
-  })
+  try {
+    const inventoryId = req.params.invId
+    const vehicle = await invModel.getInventoryByInventoryId(inventoryId)
+    
+    if (!vehicle) {
+      const err = new Error("Vehicle not found")
+      err.status = 404
+      next(err)
+      return
+    }
+    
+    let nav = await utilities.getNav()
+    res.render("./inventory/detail", {
+      title: vehicle.inv_make + " " + vehicle.inv_model,
+      nav,
+      vehicle,
+    })
+  } catch (error) {
+    error.status = 500
+    next(error)
+  }
 }
 
 module.exports = invCont
